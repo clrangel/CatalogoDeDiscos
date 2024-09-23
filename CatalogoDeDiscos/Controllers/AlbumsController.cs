fb65801f78fb5da5f9f9d59c42d5dc22627ae1ce
Camilo LR <autumndrums@hotmail.com>
2024-09-23 16:42:42 -0300

    AlbumService e FindAll basico

--- CatalogoDeDiscos/Controllers/AlbumsController.cs
+++ CatalogoDeDiscos/Controllers/AlbumsController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CatalogoDeDiscos.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CatalogoDeDiscos.Controllers
 {
     public class AlbumsController : Controller
     {
+        private readonly AlbumService _albumService;
+
+        public AlbumsController(AlbumService albumService)
+        {
+            _albumService = albumService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var list = _albumService.FindAll();
+
+            return View(list);
         }
     }
 }
